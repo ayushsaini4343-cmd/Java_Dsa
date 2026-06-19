fc6d14f5af36ee947dc16bac4103c95130a768a4
Ayush saini <ayushsaini2a6@gmail.com>
2026-06-20 02:54:26 +0530

    1st

---
+++ src/lecture1/Simple_Interest.java
@@ -0,0 +1,14 @@
+package lecture1;
+
+public class Simple_Interest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int p = 10000;
+		int r = 10;
+		int t = 1;
+		int SI = (p*r*t)/100;
+		System.out.println(SI);
+	}
+
+}
