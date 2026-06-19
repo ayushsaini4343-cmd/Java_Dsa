9aa0aa932fd9d2e06aa9cc4cf19419093b5c046a
Ayush saini <ayushsaini2a6@gmail.com>
2026-06-20 02:46:59 +0530

    1st commit test

---
+++ src/lecture1/Arithmetic_Operator.java
@@ -0,0 +1,18 @@
+package lecture1;
+
+public class Arithmetic_Operator {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int c = a+b;
+		System.out.println(c);
+		c = a-b;
+		System.out.println(c);
+		c = b/a;
+		System.out.println(c);
+		c = b%a;
+		System.out.println(c);
+	}
+
+}
